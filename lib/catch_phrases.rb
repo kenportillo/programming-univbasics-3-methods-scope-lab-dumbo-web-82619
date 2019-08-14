5a10d1761d5fd01d8ae0da77d82ae0955678e3db
Kenneth Portillo <portillokenneth@gmail.com>
2019-08-14 05:10:53 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -12,8 +12,7 @@ def link
   puts "It's Dangerous To Go Alone! Take This."
 end
 
-def any_phrase
-  puts "takes in an argument and puts out the catch phrase"
+def any_phrase(phrase)
   phrase = "Do A Barrel Roll!"
   puts phrase 
 end
